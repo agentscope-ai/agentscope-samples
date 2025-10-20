383505b2453220d911d60e1ccb31a0403c15610a
liufeihong <lfh02106008@alibaba-inc.com>
2025-10-20 15:17:05 +0800

    添加uv

---
+++ _version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""The version of agentscope samples."""
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -3,37 +3,21 @@
 import re
 import setuptools
 
-# obtain version from src/agentscope/_version.py
-with open("src/agentscope/_version.py", encoding="UTF-8") as f:
-    VERSION = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        f.read(),
-        re.MULTILINE,
-    ).group(1)
+VERSION = "0.1.0"
 
-NAME = "agentscope"
-URL = "https://github.com/agentscope-ai/agentscope"
-
+NAME = "agentscope-samples"
+URL = "https://github.com/agentscope-ai/agentscope-samples"
 minimal_requires = []
-
 extra_requires = [
-    # Model
-    "ollama",
-    "google-genai",
-    "Pillow",
-    "transformers",
-    "jinja2",
-    # Evaluator
-    "ray",
-    # Long-term memory
-    "mem0ai",
-    "packaging",
-    # RAG
-    "pypdf",
-    "nltk",
-    "qdrant-client",
+    "agentscope[dev]>=1.0.5",
+    "agentscope_runtime>=0.1.5",
+    "pyyaml>=6.0.2",
+    "quart>=0.8.0",
+    "quart-cors>=0.8.0",
+    "flask_sqlalchemy>=3.1.1",
+    "flask>=3.1.2",
+    "flask_cors>=6.0.1",
 ]
-
 dev_requires = [
     "pre-commit",
     "pytest",
@@ -42,23 +26,19 @@ dev_requires = [
     "myst_parser",
     "matplotlib",
 ]
-
 with open("README.md", "r", encoding="UTF-8") as fh:
     long_description = fh.read()
-
 setuptools.setup(
     name=NAME,
     version=VERSION,
     author="SysML team of Alibaba Tongyi Lab ",
-    author_email="gaodawei.gdw@alibaba-inc.com",
-    description="AgentScope: A Flexible yet Robust Multi-Agent Platform.",
+    description="AgentScope Sample Agents: Ready-to-use multi-agent examples built on AgentScope and AgentScope Runtime.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=URL,
     download_url=f"{URL}/archive/v{VERSION}.tar.gz",
     keywords=["deep-learning", "multi agents", "agents"],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
+    packages=[],
     install_requires=minimal_requires,
     extras_require={
         "full": minimal_requires + extra_requires,
@@ -69,6 +49,7 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
